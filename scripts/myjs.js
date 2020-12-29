b46212e28b2948df945425feb1588df0d9f060b9
Tom Delaunay <tdelaunay.lamanu@gmail.com>
2020-12-29 12:15:08 +0100

    Initialisation des Fichiers

---
+++ scripts/myjs.js
@@ -0,0 +1,18 @@
+/* ------------------------------------------------
+
+    Projet - Ecommerce
+    Tom Delaunay
+    La Manu - 2020/2021
+
+------------------------------------------------ */
+
+function js1() {
+
+}
+
+
+$(function() {
+    console.log("Chargement JS - OK");
+    js1();
+
+});
